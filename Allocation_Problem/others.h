71ac4bb3e9ed1d5378433110459635bcf4024fec
flodavid <fl.david.53@gmail.com>
2016-02-04 17:48:02 +0100

    ajout fonction max

--- Allocation_Problem/others.h
+++ Allocation_Problem/others.h
@@ -7,4 +7,9 @@
 void printdebug( const char * format, ... )
 __attribute__ ((format (printf, 1, 2)));
 
+/**
+ * return the max value of a series of int
+ */
+int max(int n, int ** values);
+
 #endif
