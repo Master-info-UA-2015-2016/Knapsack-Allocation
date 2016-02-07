6c9bb85cf5267ae436e5f40d8027c76d83641654
flodavid <fl.david.53@gmail.com>
2016-02-07 15:18:41 +0100

    ajout temps cpu

--- Knapsack_Problem/main.c
+++ Knapsack_Problem/main.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/timeb.h>
+
 #include "file.h"
 #include "knapsack.h"
 
 int main(int argc, char **argv) {
+
+    struct timeb t0, t1;    /* Timers */
+    double cpu_time;        /* Time */
+    
+    ftime(&t0);
+    
 	tab_knapsack tab_k;
 	int ** z;
 	int ** x;
-	loadInstance("exemple.txt", &(tab_k.nb_items), &(tab_k.capacity), &(tab_k.items));
+	loadInstance("./resources/exemple.txt", &(tab_k.nb_items), &(tab_k.capacity), &(tab_k.items));
 	display_tab_knapsack(&tab_k);
 	allocate_tab(&z, tab_k.capacity, tab_k.nb_items);
 	allocate_tab(&x, tab_k.capacity, tab_k.nb_items);
@@ -19,5 +27,15 @@ int main(int argc, char **argv) {
 	delete_tab(z,tab_k.capacity, tab_k.nb_items);
 	delete_tab(x,tab_k.capacity, tab_k.nb_items);
 	delete_tab_knapsack(&tab_k);
-    return 0;
+	
+	
+	// Memory liberation
+	
+
+
+    ftime(&t1);
+    cpu_time = (double)(t1.time - t0.time) + (double)(t1.millitm-t0.millitm)/1000.0;
+    printf("\nCPU time : %f seconds.\n", cpu_time);
+
+    return EXIT_SUCCESS;
 }
