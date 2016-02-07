0ffda9e22b0dd211ef1f07a1a6baa023d9f45c31
flodavid <fl.david.53@gmail.com>
2016-02-07 12:46:59 +0100

    commentaires et ajout solution optimale dans allocation__pb

--- Allocation_Problem/allocation.h
+++ Allocation_Problem/allocation.h
@@ -3,10 +3,19 @@
 
 #include "file.h"
 
+/**
+ * Initialisation of arrays, before using algorithm
+ */
 void init_dynamic_optimal(tab_allocation* tab_a, int *** z, int *** x);
 
+/*
+ * Calcul of profit by number of crates sold by location, results stored in z and x
+ */
 void dynamic_optimal(tab_allocation* tab_a, int *** z, int *** x);
 
+/*
+ * Give number of crates sold by location to obtain maximum profit
+ */
 void optimal_solution(int *** x, int ** solution, int alpha, int k);
 
 #endif
--- Allocation_Problem/main.c
+++ Allocation_Problem/main.c
@@ -29,28 +29,37 @@ int main(int argc, char **argv) {
 	display_tab_allocation(allocation);
 	printf("\n");
 
+	// Creation of arrays Z and X
 	allocate_tab(&z, allocation->nb_crates, allocation->nb_location);
 	allocate_tab(&x, allocation->nb_crates, allocation->nb_location);
+	
+	// Initialization of Z and X according to data
 	init_dynamic_optimal(allocation, &z, &x);
+	// Search of maximum profit by number f crate by location, stored into Z and X
 	dynamic_optimal(allocation, &z, &x);
 	
-	printf("\n");
+	printf("\nArray of profits\n");
+	display_tab(&x, allocation->nb_crates, allocation->nb_location);
+	printf("\nArray of number of crates sold to maximize profit\n");
 	display_tab(&z, allocation->nb_crates, allocation->nb_location);
 	printf("\n");
-	display_tab(&x, allocation->nb_crates, allocation->nb_location);
-	printf("\n");
+	
 	sol = (int*)malloc(allocation->nb_location*sizeof(int));
 	optimal_solution(&x, &sol, allocation->nb_crates-1, allocation->nb_location-1);
 	
-	for (i = 0; i < allocation->nb_location; ++i) printf("%d;", sol[i]);
-	    
+	printf("Number of crates solde by location :\n");
+	for (i = 0; i < allocation->nb_location; ++i){
+	     printf("%d ;", sol[i]);
+	}
+	
+	printf("\nOptimal solution : %d\n", z[allocation->nb_crates-1][allocation->nb_location-1] );
+	
+	
 	// Memory liberation
 	delete_tab(z, allocation->nb_crates, allocation->nb_location);
 	delete_tab(x, allocation->nb_crates, allocation->nb_location);
 	delete_tab_allocation(allocation);
 	free(allocation);
-	
-	
 
 
     ftime(&t1);
